f10b1169a7f4d9a4004a44727780ed250f770e87
Jesús Monge <jesus_jaml@outlook.com>
2021-12-27 00:12:15 -0500

    Extendiendo el Document. Podemos agregar atributos adicionales requeridos en nuestra app

--- pages/about.js
+++ pages/about.js
@@ -1,12 +1,16 @@
-import Navbar from "../components/Navbar"
+import Head from "next/head";
+import Navbar from "../components/Navbar";
 
 const About = () => {
-    return (
-        <div>
-            <Navbar/>
-            <h2>Página de About</h2>
-        </div>
-    )
-}
+  return (
+    <div>
+      <Head>
+        <title>About page</title>
+      </Head>
+      <Navbar />
+      <h2>Página de About</h2>
+    </div>
+  );
+};
 
-export default About
+export default About;
--- pages/index.js
+++ pages/index.js
@@ -1,30 +1,34 @@
+import Head from "next/head";
 import { useEffect, useState } from "react";
 import Navbar from "../components/Navbar";
 // Challenge PRODUCT_DATA filled by users
 const Home = () => {
+  const [productList, setProductList] = useState([]);
 
-    const [productList, setProductList] = useState([]);
+  useEffect(() => {
+    window
+      .fetch("/api/avo")
+      .then((res) => res.json())
+      .then(({ data, length }) => {
+        setProductList(data);
+        console.log(data);
+      });
+  }, []);
 
-    useEffect(() => {
-        window.fetch('/api/avo')
-        .then((res) => res.json())
-        .then(({data, length}) => {
-            setProductList(data)
-            console.log(data)
-        })
-    }, [])
+  return (
+    <div>
+      <Head>
+        <title>Platzi-Avo 🥑</title>
+      </Head>
+      <Navbar />
+      <h1>Hello World!</h1>
+      {productList.map((product) => (
+        <>
+          <div key={product.id}>{product.name}</div>
+        </>
+      ))}
+    </div>
+  );
+};
 
-    return (
-        <div>
-            <Navbar/>
-            <h1>Hello World!</h1>
-            {productList.map((product) => (
-                <>
-                    <div key={product.id}>{product.name}</div>
-                </>
-            ))}
-        </div>
-    )
-}
-
-export default Home
+export default Home;
