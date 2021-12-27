2f29e60bb84fd017e9ecee091f1660388192aa8f
Jesús Monge <jesus_jaml@outlook.com>
2021-12-26 22:35:38 -0500

    Requesting avos when id is truthy

--- pages/product/[id].js
+++ pages/product/[id].js
@@ -1,31 +1,35 @@
-import { useState, useEffect } from "react/cjs/react.development"
-import { useRouter } from 'next/router'
-
+import { useState, useEffect } from "react/cjs/react.development";
+import { useRouter } from "next/router";
+import Navbar from "../../components/Navbar";
 
 const ProductPage = () => {
-    const [product, setProduct] = useState()
-    
-    // console.log(product)
+  const [product, setProduct] = useState();
 
-    const { query: { id } } = useRouter();
+  // console.log(product)
 
-    useEffect(() => {
-        window
+  const {
+    query: { id },
+  } = useRouter([]);
+
+  useEffect(() => {
+    id &&
+      window
         .fetch(`/api/avo/${id}`)
         .then((response) => response.json())
-        .then(({data}) => {
-            setProduct(data)
+        .then(({ data }) => {
+          setProduct(data);
         })
-        .catch(err => console.log(err.message))
-    }, [id])
-        
-    return (
-        <div>
-            <h1>Avo name:</h1>
-            <h2>{product?.name}</h2>
-            <p>{product?.attributes.description}</p>
-        </div>
-    )
-}
+        .catch((err) => console.log(err.message));
+  }, [id]);
 
-export default ProductPage
+
+  return (
+    <>
+      <h2>{product?.name}</h2>
+      <p>{product?.attributes.description}</p>
+      <span>{product?.image}</span>
+    </>
+  );
+};
+
+export default ProductPage;
