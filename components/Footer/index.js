4fac4af09d60e605e10b0441028f9ef9b7406e1c
Jesús Monge <jesus_jaml@outlook.com>
2022-01-19 18:48:29 -0500

    Platzi Avo App Complete build frontend and backend. Add links to anchors, font color grey. Getting all ready to deploy

--- components/Footer/index.js
+++ components/Footer/index.js
@@ -1,34 +1,46 @@
 import Credits from "@components/Credits";
 import React from "react";
-import stlyes from "./styles.module.css";
+import { List } from "semantic-ui-react";
+import styles from "./styles.module.css";
 
 const Footer = () => {
   return (
-    <footer className={stlyes.footer}>
-      <div className={stlyes.links}>
-        <h4>Nosotros</h4>
-        <a href="">Conoce más</a>
-      </div>
-      <div className={stlyes.links}>
-        <h4>Servicios</h4>
-        <a href="">Todos los productos</a>
-      </div>
-      <div className={stlyes.links}>
-        <h4>Hecho con 💚</h4>
-        <a href="#">@JMongeDev</a>
-
-        <p>
-          Platzi y su curso de <a href="#">Next.JS</a> de Platzi dictado por{" "}
-          <a href="#">@jonalvarezz</a>
-        </p>
-        <div>
-          <p className={stlyes.social}>🕊️ Twitter</p>
-          <p className={stlyes.social}>🐈‍⬛ GitHub</p>
+    <>
+      <footer className={styles.footer}>
+        <div className={styles.links}>
+          <h4>Nosotros</h4>
+          <a href="#">Conoce más</a>
         </div>
-      </div>
+        <div className={styles.links}>
+          <h4>Servicios</h4>
+          <a href="#">Todos los productos</a>
+        </div>
+        <div className={styles.links}>
+          <h4>Hecho con 💚</h4>
+          <a href="#">@JMongeDev</a>
 
+          <p>
+            Platzi y su curso de <a href="#">Next.JS</a> de Platzi dictado por{" "}
+            <a href="#">@jonalvarezz</a>
+          </p>
+          <div className={styles.social}>
+            <List.Item
+              icon="twitter"
+              className={styles.icons}
+              content={<a href="#">Twitter</a>}
+            />
+            <List.Item
+              icon="github"
+              className={styles.icons}
+              content={<a href="#">Github</a>}
+            />
+            {/* <p className={styles.social}>🕊️ Twitter</p>
+            <p className={styles.social}>🐈‍⬛ GitHub</p> */}
+          </div>
+        </div>
+      </footer>
       <Credits />
-    </footer>
+    </>
   );
 };
 
